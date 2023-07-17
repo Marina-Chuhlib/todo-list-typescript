154e3ca711d175ffe7492a7f2e35bd1d44472aa3
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-07-17 15:06:28 +0300

    update basename

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/todo-list-typescript">
       <App />
     </BrowserRouter>
   </React.StrictMode>
